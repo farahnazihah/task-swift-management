3b45d9043143bf87e0fbd84d18a0dcf9b292bfe4
Farah Nazihah <farah.nazihah@gooddoctor.co.id>
2024-05-21 22:58:20 +0700

    chore: remove unused env

---
+++ src/constants/env.ts
@@ -0,0 +1,7 @@
+export const DO_BUCKET_ACCESS_KEY_ID =
+  process.env.DO_BUCKET_ACCESS_KEY_ID ?? "";
+export const DO_BUCKET_SECRET_KEY = process.env.DO_BUCKET_SECRET_KEY ?? "";
+
+export const DO_BUCKET_NAME = "task-challenge-2";
+export const DO_BUCKET_URL = "https://fra1.digitaloceanspaces.com";
+export const DO_BUCKET_URL_FULL = `https://${DO_BUCKET_NAME}.fra1.digitaloceanspaces.com`;
