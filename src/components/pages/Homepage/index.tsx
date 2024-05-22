09b92d5f504ad5d317813698a8a62dd4348b3f78
Farah Nazihah <farah.nazihah@gooddoctor.co.id>
2024-05-22 11:48:03 +0700

    chore: move home page component

--- src/components/pages/Homepage/index.tsx
+++ src/components/pages/Homepage/index.tsx
@@ -1,15 +1,59 @@
-import { TextArea, TextInput } from "@/components/molecules";
-import { FileUploadModal } from "@/components/organisms";
+"use client";
 
-export default function Homepage() {
+import { Modal } from "@/components/atoms";
+import { TextArea, TextInput } from "@/components/molecules";
+import { SelectFile } from "@/components/organisms";
+
+import { useState } from "react";
+
+export const HomePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<string | null>();
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSelectFile = (url: string) => {
+    setSelectedFile(url);
+    setIsModalOpen(false);
+  };
+
   return (
     <main className="w-full flex items-center justify-center h-screen bg-white text-black">
       <div className="flex flex-col p-8 gap-4 items-center justify-center w-[600px] ">
-        fasdfasdf
         <TextInput placeholder="Enter Your Name" label="Name" />
         <TextArea placeholder="Enter Your Name" label="Description" />
-        <FileUploadModal />
+        <div className="flex gap-2 items-center w-full">
+          <text className="mr-auto break-words">
+            Selected File:{" "}
+            {selectedFile ? (
+              <a className="font-bold" href={selectedFile} target="_blank">
+                {selectedFile?.split("/").pop()}
+              </a>
+            ) : (
+              "-"
+            )}
+          </text>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="btn btn-primary"
+          >
+            Upload
+          </button>
+          {selectedFile && (
+            <button
+              onClick={() => setSelectedFile(null)}
+              className="btn btn-error btn-outline"
+            >
+              Remove
+            </button>
+          )}
+        </div>
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <SelectFile onFileSelected={handleSelectFile} />
+        </Modal>
       </div>
     </main>
   );
-}
+};
