b92cf47e9c6fc9079a294acbb8f2a4d2dd8773c3
Farah Nazihah <farahnazihah102@gmail.com>
2024-05-23 12:13:06 +0700

    fix: change text tag to p tag

--- src/components/molecules/TextInput/index.tsx
+++ src/components/molecules/TextInput/index.tsx
@@ -1,4 +1,4 @@
-import { twMerge } from 'tailwind-merge';
+import { twMerge } from "tailwind-merge";
 
 interface TextInputProps {
   label: string;
@@ -9,11 +9,11 @@ const TextInput = ({ placeholder, className, label }: TextInputProps) => {
   return (
     <>
       <div className="flex flex-col gap-1 w-full">
-        <text className="pl-1">{label}</text>
+        <p className="pl-1">{label}</p>
         <input
           type="text"
           placeholder={placeholder}
-          className={twMerge('input input-bordered', className)}
+          className={twMerge("input input-bordered", className)}
         />
       </div>
     </>
